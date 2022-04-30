3c39d418d1a8c39e785cde8d2e01cccf68f86038
sergget <sergget.yang@outlook.com>
2022-04-30 21:25:58 +0800

    增加多字符串搜索排除

--- 04_URL_verifying/url_verifying.py
+++ 04_URL_verifying/url_verifying.py
@@ -5,6 +5,9 @@ def url_detect():
     src_path = "input.txt"
     dst_path = "export.txt"
     error_path = "error.txt"
+    
+    # 例外中的内容无论如何都会被写入export.txt
+    exceptions = ["google.com","steam","github.com"]
 
     export = open(dst_path,'w',encoding="utf-8")
     error = open(error_path,'w',encoding="utf-8")
@@ -15,14 +18,19 @@ def url_detect():
                 break
             else:
                 print("Verifying "+url)
-                try:
-                    r = requests.get(url,timeout=5)
-                    if r.status_code == 200:
-                        export.write(url+"\n")
-                    else:
-                        error.write("Status code: %s, url: %s\n" % (r.status_code,url))
-                except:
-                    error.write("Unreachable: %s\n" % url)
+
+                # 检查字符串中是否包含多个字符串中的任意一个
+                if any(w in url for w in exceptions):
+                    export.write(url)
+                else:
+                    try:
+                        r = requests.get(url,timeout=5)
+                        if r.status_code == 200:
+                            export.write(url+"\n")
+                        else:
+                            error.write("Status code: %s, url: %s\n" % (r.status_code,url))
+                    except:
+                        error.write("Unreachable: %s\n" % url)
     export.close()
     error.close()
 
